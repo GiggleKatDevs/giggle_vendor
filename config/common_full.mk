9712fb8b5846da6e8c238b253757dd52495cb0d8
GiggleKat <gigglekatrom@gmail.com>
2014-10-16 14:17:45 -0700

    changed vendor dir

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,8 +1,8 @@
 # Inherit common CM stuff
-$(call inherit-product, vendor/bliss/config/common.mk)
+$(call inherit-product, vendor/gigglekat/config/common.mk)
 
 # Include CM audio files
-include vendor/bliss/config/bean_audio.mk
+include vendor/giggleakat/config/bean_audio.mk
 
 # Optional CM packages
 PRODUCT_PACKAGES += \
