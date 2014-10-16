76b3459e647c38eab69ad224614b2a855bd42daf
GiggleKat <gigglekatrom@gmail.com>
2014-10-16 14:21:05 -0700

    changed vendor dir

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -1,8 +1,8 @@
 # Inherit common CM stuff
-$(call inherit-product, vendor/bliss/config/common.mk)
+$(call inherit-product, vendor/gigglekat/config/common.mk)
 
 # Include CM audio files
-include vendor/bliss/config/bliss_audio.mk
+include vendor/gigglekat/config/gigglekat_audio.mk
 
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -12,7 +12,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
-        vendor/bliss/prebuilt/common/bootanimation/320.zip:system/media/bootanimation.zip
+        vendor/gigglekat/prebuilt/common/bootanimation/320.zip:system/media/bootanimation.zip
 endif
 
-$(call inherit-product, vendor/bliss/config/telephony.mk)
+$(call inherit-product, vendor/gigglekat/config/telephony.mk)
