41b14795a21204ae925d7bdfd1614de16f33ed9d
GiggleKat <gigglekatrom@gmail.com>
2014-10-18 23:48:16 -0700

    fixed typo

--- config/common.mk
+++ config/common.mk
@@ -144,7 +144,7 @@ PRODUCT_COPY_FILES += \
     vendor/gigglekat/proprietary/Term.apk:system/app/Term.apk \
     vendor/gigglekat/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so \
     vendor/gigglekat/prebuilt/viper/ViPER4Android.apk:system/app/ViPER4Android.apk \
-    vendor/gigglekat/prebuilt/viper/libv4a_fx_ics.so:system/lib/soundfx/libv4a_fx_ics.so    
+    vendor/gigglekat/prebuilt/viper/libv4a_fx_ics.so:system/lib/soundfx/libv4a_fx_ics.so \   
     vendor/gigglekat/prebuilt/appsetting.apk:system/app/appsettings.apk \
     vendor/gigglekat/prebuilt/xposed_installer.apk:system/app/xposed_installer.apk \
     vendor/gigglekat/prebuilt/gigglekat_added/AdAway_v2.9.2.apk:system/app/AdAway.apk \
