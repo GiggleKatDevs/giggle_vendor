b81b9b8b65091eaa49843cbbc1cc26ed1a49329c
GiggleKat <gigglekatrom@gmail.com>
2014-10-26 17:32:53 -0700

    moved the libgif.so make rule

--- config/common.mk
+++ config/common.mk
@@ -135,10 +135,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/gigglekat/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
 
-# Copy libgif for Nova Launcher 3.1
-PRODUCT_COPY_FILES += \
-    vendor/gigglekat/prebuilt/common/lib/libgif.so:system/lib/libgif.so
-
 PRODUCT_COPY_FILES += \
     vendor/gigglekat/prebuilt/common/etc/init.local.rc:root/init.cm.rc
 
@@ -148,6 +144,7 @@ PRODUCT_COPY_FILES += \
     vendor/gigglekat/prebuilt/common/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so \
     vendor/gigglekat/prebuilt/viper/ViPER4Android.apk:system/app/ViPER4Android.apk \
     vendor/gigglekat/prebuilt/viper/libv4a_fx_ics.so:system/lib/soundfx/libv4a_fx_ics.so \
+    vendor/gigglekat/prebuilt/common/lib/libgif.so:system/lib/libgif.so \
     vendor/gigglekat/prebuilt/appsetting.apk:system/app/appsettings.apk \
     vendor/gigglekat/prebuilt/xposed_installer.apk:system/app/xposed_installer.apk \
     vendor/gigglekat/prebuilt/gigglekat_added/AdAway_v2.9.2.apk:system/app/AdAway.apk \
