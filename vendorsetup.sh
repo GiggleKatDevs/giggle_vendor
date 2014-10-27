ee68cce77e8b5e5c30dfd4c1384c0be27abf617b
GiggleKat <gigglekatrom@gmail.com>
2014-10-27 10:11:45 -0700

    added devices to lunch combo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,3 +6,9 @@ done
 add_lunch_combo gigglekat_vs980-userdebug
 add_lunch_combo gigglekat_moto_msm8960-userdebug
 add_lunch_combo gigglekat_ghost-userdebug
+add_lunch_combo gigglekat_hammerhead-userdebug
+add_lunch_combo gigglekat_d801-userdebug
+add_lunch_combo gigglekat_d802-userdebug
+add_lunch_combo gigglekat_m7-userdebug
+add_lunch_combo gigglekat_m8-userdebug
+
